bdfa1464ad13f72eeeff25ecaa0341e1ddc6c265
TharushaRashmina <tharusharashmina09@gmail.com>
2025-07-25 17:47:33 +0530

    fontend initialized

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className=''>
+      <BrowserRouter>
+      <Routes path='/*'>
+      <Route path="/login" element={<LoginPage/>}/>
+      
+      
+      </Routes>
+      
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
