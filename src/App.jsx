bbc846a1d0f178713b2fad85fac549ec3281da6c
TharushaRashmina <tharusharashmina09@gmail.com>
2025-07-25 20:13:33 +0530

    loginpage componenet created

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'; 
+import LoginPage from './pages/home/LoginPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +13,7 @@ function App() {
   return (
     <div className=''>
       <BrowserRouter>
+      <Toaster position="top-right"/>
       <Routes path='/*'>
       <Route path="/login" element={<LoginPage/>}/>
       
